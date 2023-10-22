1fcdb6a3b3909b2a82b9d15c416e9e3ca0018f0b
Arek525 <146826342+Arek525@users.noreply.github.com>
2023-10-22 18:04:57 +0200

    Delete zad4.py

--- zad4.py
+++
@@ -1,8 +0,0 @@
-suma = 0
-while True:
-    skladnik = float(input("Podaj liczbe: "))
-    if skladnik == 0:
-        print("Laczna suma wynosi: " + str(suma))
-        exit()
-    else:
-        suma += skladnik
